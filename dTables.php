89b734bd5c164a3fd58352fd5c61f836cecea2c5
andre stanca <stanca@xivivc.com>
2013-06-21 13:54:54 +0300

    improved performance

--- dTables.php
+++ dTables.php
@@ -5,6 +5,8 @@
  * @author andrei
  */
 
+namespace root\mainBundle\Controller;
+
 
 class dTables {
     
@@ -63,9 +65,9 @@ class dTables {
     private function sLimit()
     {        
         if ( isset( $_GET['iDisplayStart'] ) && $_GET['iDisplayLength'] != '-1' )
-	{
-		$this->sLimit = "LIMIT ". $_GET['iDisplayStart'] .", ".$_GET['iDisplayLength'] ;
-	}  
+{
+$this->sLimit = "LIMIT ". $_GET['iDisplayStart'] .", ".$_GET['iDisplayLength'] ;
+}  
         else
             $this->sLimit = '';
     }
@@ -78,23 +80,23 @@ class dTables {
     private function iSortCol()
     {        
         if ( isset( $_GET['iSortCol_0'] ) )
-	{
-		$this->sOrder = "ORDER BY  ";
-		for ( $i=0 ; $i<intval( $_GET['iSortingCols'] ) ; $i++ )
-		{
-			if ( $_GET[ 'bSortable_'.intval($_GET['iSortCol_'.$i]) ] == "true" )
-			{
-				$this->sOrder .= $this->aColumns[ intval( $_GET['iSortCol_'.$i] ) ]."
-				 	". $_GET['sSortDir_'.$i]  .", ";
-			}
-		}
-		
-		$this->sOrder = substr_replace( $this->sOrder, "", -2 );
-		if ( $this->sOrder == "ORDER BY" )
-		{
-			$this->sOrder = "";
-		}
-	}
+{
+$this->sOrder = "ORDER BY  ";
+for ( $i=0 ; $i<intval( $_GET['iSortingCols'] ) ; $i++ )
+{
+if ( $_GET[ 'bSortable_'.intval($_GET['iSortCol_'.$i]) ] == "true" )
+{
+$this->sOrder .= $this->aColumns[ intval( $_GET['iSortCol_'.$i] ) ]."
+ ". $_GET['sSortDir_'.$i]  .", ";
+}
+}
+
+$this->sOrder = substr_replace( $this->sOrder, "", -2 );
+if ( $this->sOrder == "ORDER BY" )
+{
+$this->sOrder = "";
+}
+}
         else
         {
             $this->sOrder = '';
@@ -111,16 +113,16 @@ class dTables {
         /*
          * rows filtering
          */
-	if ( $_GET['sSearch'] != "" )
-	{
-		$this->sWhere = "WHERE (";
-		for ( $i=0 ; $i<count($this->aColumns) ; $i++ )
-		{
-			$this->sWhere .= $this->aColumns[$i]." LIKE '%". $_GET['sSearch'] ."%' OR ";
-		}
-		$this->sWhere = substr_replace( $this->sWhere, "", -3 );
-		$this->sWhere .= ')';
-	} 
+if ( $_GET['sSearch'] != "" )
+{
+$this->sWhere = "WHERE (";
+for ( $i=0 ; $i<count($this->aColumns) ; $i++ )
+{
+$this->sWhere .= $this->aColumns[$i]." LIKE '%". $_GET['sSearch'] ."%' OR ";
+}
+$this->sWhere = substr_replace( $this->sWhere, "", -3 );
+$this->sWhere .= ')';
+} 
         else
         {
             $this->sOrder = '';
@@ -130,21 +132,21 @@ class dTables {
         /*
          * Column filtering
          */
-	for ( $i=0 ; $i<count($this->aColumns) ; $i++ )
-	{
-		if ( $_GET['bSearchable_'.$i] == "true" && $_GET['sSearch_'.$i] != '' )
-		{
-			if ( $this->sWhere == "" )
-			{
-				$this->sWhere = "WHERE ";
-			}
-			else
-			{
-				$this->sWhere .= " AND ";
-			}
-			$this->sWhere .= $this->aColumns[$i]." LIKE '%".$_GET['sSearch_'.$i]."%' ";
-		}
-	}
+for ( $i=0 ; $i<count($this->aColumns) ; $i++ )
+{
+if ( $_GET['bSearchable_'.$i] == "true" && $_GET['sSearch_'.$i] != '' )
+{
+if ( $this->sWhere == "" )
+{
+$this->sWhere = "WHERE ";
+}
+else
+{
+$this->sWhere .= " AND ";
+}
+$this->sWhere .= $this->aColumns[$i]." LIKE '%".$_GET['sSearch_'.$i]."%' ";
+}
+}
     }
     
     /**
@@ -154,11 +156,11 @@ class dTables {
     private function sQuery()
     {
         $this->sQuery = "
-		SELECT SQL_CALC_FOUND_ROWS ".str_replace(" , ", " ", implode(", ", $this->aColumns))."
-		FROM   $this->sTable
-		$this->sWhere
-		$this->sOrder
-		$this->sLimit";
+SELECT SQL_CALC_FOUND_ROWS ".str_replace(" , ", " ", implode(", ", $this->aColumns))."
+FROM   $this->sTable
+$this->sWhere
+$this->sOrder
+$this->sLimit";
     }
     
     
@@ -174,49 +176,46 @@ class dTables {
         /*
          * Data length after sFilter
          */
-        $this->sQuery = "SELECT FOUND_ROWS()";
+        $this->sQuery = "
+SELECT FOUND_ROWS()
+";
         $this->aResultFilterTotal = $this->PDO->fetchAll($this->sQuery);
-		$this->iFilteredTotal = $this->aResultFilterTotal[0]['FOUND_ROWS()'];
+$this->iFilteredTotal = $this->aResultFilterTotal[0]['FOUND_ROWS()'];
         
         /*
          * Total data length
          */
         $this->sQuery = "
-							SELECT COUNT(".$this->sIndexColumn.")
-							FROM   $this->sTable
-						";
+SELECT COUNT(".$this->sIndexColumn.")
+FROM   $this->sTable
+";
         
         $this->aResultTotal = $this->PDO->fetchAll($this->sQuery);
         $this->iTotal = $this->aResultTotal[0]['COUNT(id)'];
 
         /*
-		 * Output Format
-		 */
-		$this->output = array(
-			"sEcho" => intval($_GET['sEcho']),
-			"iTotalRecords" => $this->iTotal,
-			"iTotalDisplayRecords" => $this->iFilteredTotal,
-			"aaData" => array()
-		);
+ * Output Format
+ */
+$this->output = array(
+"sEcho" => intval($_GET['sEcho']),
+"iTotalRecords" => $this->iTotal,
+"iTotalDisplayRecords" => $this->iFilteredTotal,
+"aaData" => array()
+);
         
         
         foreach($this->rResult as $aRow)
         {
             for ( $i=0 ; $i<count($this->aColumns) ; $i++ )
-			{
-				if ( $this->aColumns[$i] == "version" )
-				{
-					/* Special output formatting for 'version' column */
-					$row[] = ($aRow[ $this->aColumns[$i] ]=="0") ? '-' : $aRow[ $this->aColumns[$i] ];
-				}
-				else if ( $this->aColumns[$i] != ' ' )
-				{
-					/* General output */
-					$row[] = $aRow[ $this->aColumns[$i] ];
-				}
-			}
-			$this->output['aaData'][] = $row;
+{
+$row[] = $aRow[ $this->aColumns[$i] ];
+
+}
+$this->output['aaData'][] = $row;
+                $row = array();
+
         }
+        
     }
     
 }
